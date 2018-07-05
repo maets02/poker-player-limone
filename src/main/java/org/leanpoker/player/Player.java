46b1576c0fffb3a248b7f4eac35ef4fd5635f449
chroonag <test@test>
2018-07-05 14:09:33 +0200

    mit logik

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,17 +1,28 @@
 package org.leanpoker.player;
 
-import com.google.gson.JsonElement;
+import java.util.Iterator;
 
-import java.util.Map;
-import java.util.UUID;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 
 public class Player {
 
     static final String VERSION = "0.0.3";
 
     public static int betRequest(JsonElement request) {
-        System.out.print(request);
-        return 236;
+    	Iterator<JsonElement> iterator = request.getAsJsonObject().getAsJsonArray("players").iterator();
+        int bet = 0;
+        while(iterator.hasNext()) {
+            JsonElement currentPlayer = iterator.next();
+            JsonPrimitive playerName = currentPlayer.getAsJsonObject().getAsJsonPrimitive("name");
+            if ("limone".equals(playerName.getAsString())){
+                bet = currentPlayer.getAsJsonObject().getAsJsonPrimitive("bet").getAsInt();
+            }
+        }
+        int currentBuyIn = request.getAsJsonObject().getAsJsonPrimitive("current_buy_in").getAsInt();
+        int minimumBuyIn = currentBuyIn - bet;
+        int small_blind = request.getAsJsonObject().getAsJsonPrimitive("small_blind").getAsInt();
+        return minimumBuyIn < small_blind ? small_blind : minimumBuyIn;
     }
 
     public static void showdown(JsonElement game) {
