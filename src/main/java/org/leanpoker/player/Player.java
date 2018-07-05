e6020e63c7a184dff816cb0bfc73e4f915e24fc8
chroonag <test@test>
2018-07-05 15:00:51 +0200

    006i

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,7 +10,7 @@ import com.google.gson.JsonPrimitive;
 
 public class Player {
 
-    static final String VERSION = "0.0.5";
+    static final String VERSION = "0.0.6";
 
     public static int betRequest(JsonElement request) {
         int round = request.getAsJsonObject().getAsJsonPrimitive("round").getAsInt();
@@ -33,7 +34,7 @@ public class Player {
         if (holeCardRanks.size() == 2) {
             String rank1 = holeCardRanks.get(0);
             String rank2 = holeCardRanks.get(1);
-            if (rank1.equals(rank2)) {
+            if (keepPlaying(rank1, rank2)) {
                 int currentBuyIn = request.getAsJsonObject().getAsJsonPrimitive("current_buy_in").getAsInt();
                 int minimumBuyIn = currentBuyIn - bet;
                 int small_blind = request.getAsJsonObject().getAsJsonPrimitive("small_blind").getAsInt();
@@ -41,6 +42,11 @@ public class Player {
             }
         }
        return 0;
+    }
+
+    private static boolean keepPlaying(String rank1, String rank2) {
+        List<String> l = Arrays.asList("AA","KK", "QQ", "JJ", "1010", "99", "88", "77", "AK", "AQ", "AJ", "A10", "KQ", "KJ", "K10", "QJ", "Q10", "J10", "J9", "109");
+        return l.contains(rank1 + rank2) || l.contains(rank2 + rank1);
     }
 
     public static void showdown(JsonElement game) {
